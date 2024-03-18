e6ad95d2c7dd4518c1e05d03c2ae535371780e0d
rajkumaratvidyamandira <das.rajkumar.vidyamandira@gmail.com>
2024-03-18 12:53:24 +0530

    18_03_2024

--- DSA/DSA(ANJ)/new_file.py
+++ DSA/DSA(ANJ)/new_file.py
@@ -1,2 +1,15 @@
-print("helo world")
-will i make a infinity loop?
+import math
+import matplotlib.pyplot as plt
+import numpy
+
+def f(x):
+    return x*x
+
+a = numpy.arange(100)
+
+b = f(a)
+
+plt.plot(a,b)
+plt.xlabel("x")
+plt.ylabel("X^2")
+plt.show()
---
+++ DSA/DSA(ANJ)/syllebus.c
@@ -0,0 +1,9 @@
+// 29/03/2024
+// asymtotic notation
+// stack 
+//     infix to postfix
+// recoursion
+
+// queue
+
+ 
